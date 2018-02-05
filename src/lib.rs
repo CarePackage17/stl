3f3d228b05c895b43c33461e877d82dd0cf45b06
ArmsOfSorrow <wave.dev@hotmail.com>
2018-02-05 17:18:16 +0100

    removed some unnecessary trial-and-error code

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,12 @@
 #[cfg(test)]
 mod tests {
-    use nom::{IResult, float};
     use super::*;
-
-    #[test]
-    fn parse_test() {
-        let s = "testssssss";
-        let res = test_parser(s.as_bytes());
-        match res {
-            IResult::Done(_, output) => {
-                assert_eq!("test".as_bytes(), output);
-            },
-            _ => assert!(false)
-        }
-    }
-
-    #[test]
-    fn parse_float() {
-        let x = "65.25";
-        match float(x.as_bytes()) {
-            IResult::Done(_, output) => {
-                assert_eq!(65.25f32, output);
-            },
-            _ => assert!(false)
-        }
-    }
 
     #[test]
     fn parse_vertex() {
         let vertex = "vertex 1.0 2.3 3.4";
 
-        //since the wrapped value is a (I, O) we only need the O to check
-        let parsed = vertex_parser(vertex.as_bytes()).unwrap().1;
-        assert_eq!(parsed.0, "vertex".as_bytes());
-        assert_eq!(parsed.1, 1.0f32);
-        assert_eq!(parsed.2, 2.3f32);
-        assert_eq!(parsed.3, 3.4f32);
-    }
-
-    #[test]
-    fn parse_vertex_without_string() {
-        let vertex = "vertex 1.0 2.3 3.4";
-
-        let tuple = vertex_without_string(vertex.as_bytes()).unwrap().1;
+        let tuple = vertex_parser(vertex.as_bytes()).unwrap().1;
         assert_eq!(tuple.0, 1.0f32);
         assert_eq!(tuple.1, 2.3f32);
         assert_eq!(tuple.2, 3.4f32);
@@ -130,15 +94,11 @@ impl Vertex {
 
 named!(pub test_parser, tag!("test"));
 
-//a vertex record looks like "vertex 3.3 4.4 5.5", so we return a byte slice alongside 3 floats...but wait, actually
-//we only need to match it, not return.
-named!(pub vertex_parser<&[u8], (&[u8], f32, f32, f32)>, ws!(tuple!(tag!("vertex"), float, float, float)));
-
-named!(pub vertex_without_string<&[u8], (f32, f32, f32)>, ws!(preceded!(tag!("vertex"), tuple!(float, float, float))));
+named!(pub vertex_parser<&[u8], (f32, f32, f32)>, ws!(preceded!(tag!("vertex"), tuple!(float, float, float))));
 
 named!(pub normal_parser<&[u8], (f32, f32, f32)>, ws!(preceded!(tag!("normal"), tuple!(float, float, float))));
 
-named!(pub vertex_with_custom_type<&[u8], Vertex>, map!(vertex_without_string, Vertex::new));
+named!(pub vertex_with_custom_type<&[u8], Vertex>, map!(vertex_parser, Vertex::new));
 
 named!(pub triangle_parser<&[u8], (Vertex, Vertex, Vertex)>, ws!(tuple!(vertex_with_custom_type, vertex_with_custom_type, vertex_with_custom_type)));
 
