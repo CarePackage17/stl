cdb376408eb039ccd189c60ba0c4c2d9faf92f6a
ArmsOfSorrow <wave.dev@hotmail.com>
2018-02-05 17:13:56 +0100

    added handling for loop-endloop items

--- src/lib.rs
+++ src/lib.rs
@@ -78,6 +78,25 @@ mod tests {
         assert_eq!(triangle.2.y, 40.1f32);
         assert_eq!(triangle.2.z, 22.3f32);
     }
+
+    #[test]
+    fn parse_loop_record() {
+        let loop_str = b"outer loop\nvertex 1.0 2.3 3.4\nvertex 2.2 2.5 3.9\nvertex 3.5 40.1 22.3\nendloop";
+
+        let triangle = loop_record(loop_str).unwrap().1;
+
+        assert_eq!(triangle.0.x, 1.0f32);
+        assert_eq!(triangle.0.y, 2.3f32);
+        assert_eq!(triangle.0.z, 3.4f32);
+
+        assert_eq!(triangle.1.x, 2.2f32);
+        assert_eq!(triangle.1.y, 2.5f32);
+        assert_eq!(triangle.1.z, 3.9f32);
+
+        assert_eq!(triangle.2.x, 3.5f32);
+        assert_eq!(triangle.2.y, 40.1f32);
+        assert_eq!(triangle.2.z, 22.3f32);
+    }
 }
 
 #[macro_use]
@@ -117,6 +136,15 @@ named!(pub vertex_parser<&[u8], (&[u8], f32, f32, f32)>, ws!(tuple!(tag!("vertex
 
 named!(pub vertex_without_string<&[u8], (f32, f32, f32)>, ws!(preceded!(tag!("vertex"), tuple!(float, float, float))));
 
+named!(pub normal_parser<&[u8], (f32, f32, f32)>, ws!(preceded!(tag!("normal"), tuple!(float, float, float))));
+
 named!(pub vertex_with_custom_type<&[u8], Vertex>, map!(vertex_without_string, Vertex::new));
 
 named!(pub triangle_parser<&[u8], (Vertex, Vertex, Vertex)>, ws!(tuple!(vertex_with_custom_type, vertex_with_custom_type, vertex_with_custom_type)));
+
+//we also need to deal with outer loop and endloop words
+named!(pub loop_record<&[u8], (Vertex, Vertex, Vertex)>, ws!(delimited!(tag!("outer loop"), triangle_parser, tag!("endloop"))));
+
+//single facet needs a custom return type that wraps the normal and the 3 vertices
+
+//for parsing multiple facets whe should use many0
