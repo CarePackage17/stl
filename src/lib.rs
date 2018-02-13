88b9a25d767a12f99dbf9ed431331cfbea0ac9fd
ArmsOfSorrow <wave.dev@hotmail.com>
2018-02-13 20:53:00 +0100

    Lots of cleanup and custom structs.

--- src/lib.rs
+++ src/lib.rs
@@ -55,37 +55,26 @@ mod tests {
 
     #[test]
     fn parse_facet() {
-        let facet_bytes = b"normal 0.2 0.3 0.4\nouter loop\nvertex 1.0 2.3 3.4\nvertex 2.2 2.5 3.9\nvertex 3.5 40.1 22.3\nendloop";
-
-        let facet = facet_parser(facet_bytes).unwrap().1;
-        assert_eq!(facet.0.x, 0.2f32);
-        assert_eq!(facet.0.y, 0.3f32);
-        assert_eq!(facet.0.z, 0.4f32);
-    }
-
-    #[test]
-    fn parse_facet_complete() {
         let facet_bytes = b"facet  normal 0.2 0.3 0.4\nouter loop\nvertex 1.0 2.3 3.4\nvertex 2.2 2.5 3.9\nvertex 3.5 40.1 22.3\nendloop endfacet";
 
-        //in this case facet is a Vertex 4-tuple
-        let four_tuple = facet_parser_complete(facet_bytes).unwrap().1;
+        let facet = facet_parser_struct(facet_bytes).unwrap().1;
 
         //normal
-        assert_eq!(four_tuple.0.x, 0.2f32);
-        assert_eq!(four_tuple.0.y, 0.3f32);
-        assert_eq!(four_tuple.0.z, 0.4f32);
+        assert_eq!(facet.normal.x, 0.2f32);
+        assert_eq!(facet.normal.y, 0.3f32);
+        assert_eq!(facet.normal.z, 0.4f32);
         //vertex 1
-        assert_eq!(four_tuple.1.x, 1.0f32);
-        assert_eq!(four_tuple.1.y, 2.3f32);
-        assert_eq!(four_tuple.1.z, 3.4f32);
+        assert_eq!(facet.a.x, 1.0f32);
+        assert_eq!(facet.a.y, 2.3f32);
+        assert_eq!(facet.a.z, 3.4f32);
         //vertex 2
-        assert_eq!(four_tuple.2.x, 2.2f32);
-        assert_eq!(four_tuple.2.y, 2.5f32);
-        assert_eq!(four_tuple.2.z, 3.9f32);
+        assert_eq!(facet.b.x, 2.2f32);
+        assert_eq!(facet.b.y, 2.5f32);
+        assert_eq!(facet.b.z, 3.9f32);
         //vertex 3
-        assert_eq!(four_tuple.3.x, 3.5f32);
-        assert_eq!(four_tuple.3.y, 40.1f32);
-        assert_eq!(four_tuple.3.z, 22.3f32);
+        assert_eq!(facet.c.x, 3.5f32);
+        assert_eq!(facet.c.y, 40.1f32);
+        assert_eq!(facet.c.z, 22.3f32);
     }
 
     #[test]
@@ -93,20 +82,20 @@ mod tests {
         let facet_bytes = b"facet  normal 0.2 0.3 0.4\nouter loop\nvertex 1.0 2.3 3.4\nvertex 2.2 2.5 3.9\nvertex 3.5 40.1 22.3\nendloop endfacet
                             \nfacet normal 55.5 66.6 77.7\nouter loop\nvertex 10.1 11.1 12.3\nvertex 20.2 21.3 24.6\nvertex 3.3 6.9 4.7\nendloop endfacet";
 
-        let vec = facet_list(facet_bytes).unwrap().1;
+        let vec = facet_list_struct(facet_bytes).unwrap().1;
         assert_eq!(vec.len(), 2);
 
         let first = &vec[0];
         let second = &vec[1];
 
         //first face normal
-        assert_eq!(first.0.x, 0.2f32);
-        assert_eq!(first.0.y, 0.3f32);
-        assert_eq!(first.0.z, 0.4f32);
+        assert_eq!(first.normal.x, 0.2f32);
+        assert_eq!(first.normal.y, 0.3f32);
+        assert_eq!(first.normal.z, 0.4f32);
 
-        assert_eq!(second.0.x, 55.5f32);
-        assert_eq!(second.0.y, 66.6f32);
-        assert_eq!(second.0.z, 77.7f32);
+        assert_eq!(second.normal.x, 55.5f32);
+        assert_eq!(second.normal.y, 66.6f32);
+        assert_eq!(second.normal.z, 77.7f32);
     }
 
     #[test]
@@ -123,21 +112,21 @@ mod tests {
         let verts = parse_ascii_stl(bytes).unwrap().1;
 
         let first = &verts[0];
-        assert_eq!(first.0.x, 0.0f32);
-        assert_eq!(first.0.y, 0.0f32);
-        assert_eq!(first.0.z, -1.0f32);
+        assert_eq!(first.normal.x, 0.0f32);
+        assert_eq!(first.normal.y, 0.0f32);
+        assert_eq!(first.normal.z, -1.0f32);
 
-        assert_eq!(first.1.x, 0.0f32);
-        assert_eq!(first.1.y, 0.0f32);
-        assert_eq!(first.1.z, 0.0f32);
+        assert_eq!(first.a.x, 0.0f32);
+        assert_eq!(first.a.y, 0.0f32);
+        assert_eq!(first.a.z, 0.0f32);
 
-        assert_eq!(first.2.x, 1.0f32);
-        assert_eq!(first.2.y, 1.0f32);
-        assert_eq!(first.2.z, 0.0f32);
+        assert_eq!(first.b.x, 1.0f32);
+        assert_eq!(first.b.y, 1.0f32);
+        assert_eq!(first.b.z, 0.0f32);
 
-        assert_eq!(first.3.x, 1.0f32);
-        assert_eq!(first.3.y, 0.0f32);
-        assert_eq!(first.3.z, 0.0f32);
+        assert_eq!(first.c.x, 1.0f32);
+        assert_eq!(first.c.y, 0.0f32);
+        assert_eq!(first.c.z, 0.0f32);
     }
 }
 
@@ -146,11 +135,11 @@ extern crate nom;
 
 mod data;
 
-use data::Vertex;
+use data::{Vertex, Facet};
 use nom::{float, is_space, alpha};
 
 
-named!(pub vertex_parser<&[u8], Vertex>, 
+named!(vertex_parser<&[u8], Vertex>, 
     map!(
         ws!(
             preceded!(
@@ -161,7 +150,7 @@ named!(pub vertex_parser<&[u8], Vertex>,
     )
 );
 
-named!(pub normal_parser<&[u8], Vertex>, 
+named!(normal_parser<&[u8], Vertex>, 
     map!(
         ws!(
             preceded!(
@@ -172,20 +161,17 @@ named!(pub normal_parser<&[u8], Vertex>,
     )
 );
 
-named!(pub triangle_parser<&[u8], (Vertex, Vertex, Vertex)>,
+named!(triangle_parser<&[u8], (Vertex, Vertex, Vertex)>,
     ws!(
         tuple!(vertex_parser, vertex_parser, vertex_parser)
     )
 );
 
 //we also need to deal with outer loop and endloop words
-named!(pub loop_record<&[u8], (Vertex, Vertex, Vertex)>, ws!(delimited!(tag!("outer loop"), triangle_parser, tag!("endloop"))));
-
-//single facet needs a custom return type that wraps the normal and the 3 vertices
-named!(pub facet_parser<&[u8], (Vertex, (Vertex, Vertex, Vertex))>, ws!(tuple!(normal_parser, loop_record)));
+named!(loop_record<&[u8], (Vertex, Vertex, Vertex)>, ws!(delimited!(tag!("outer loop"), triangle_parser, tag!("endloop"))));
 
 //do_parse! seems to be the solution for multiple subparser chaining and result aggregation
-named!(pub facet_parser_complete<&[u8], (Vertex, Vertex, Vertex, Vertex)>,
+named!(facet_parser_complete<&[u8], (Vertex, Vertex, Vertex, Vertex)>,
     ws!(
         do_parse!(
             tag!("facet") >>
@@ -197,9 +183,15 @@ named!(pub facet_parser_complete<&[u8], (Vertex, Vertex, Vertex, Vertex)>,
     )
 );
 
-named!(pub facet_list<&[u8], Vec<(Vertex, Vertex, Vertex, Vertex)>>,
+named!(facet_parser_struct<&[u8], Facet>,
+    map!(
+        facet_parser_complete, Facet::from_tuple
+    )
+);
+
+named!(facet_list_struct<&[u8], Vec<Facet>>,
     ws!(
-        many0!(facet_parser_complete)
+        many0!(facet_parser_struct)
     )
 );
 
@@ -223,14 +215,11 @@ named!(parse_ending<&[u8], Option<&[u8]>>,
     )
 );
 
-named!(parse_ascii_stl<&[u8], Vec<(Vertex, Vertex, Vertex, Vertex)>>,
+named!(pub parse_ascii_stl<&[u8], Vec<Facet>>,
     do_parse!(
         parse_beginning >>
-        facets: facet_list >>
+        facets: facet_list_struct >>
         parse_ending >>
         (facets)
     )
 );
-
-//we need to verify! if the name matches at the beginning/end
-//also, we should probably return custom structs instead of tuples (readability and such)
